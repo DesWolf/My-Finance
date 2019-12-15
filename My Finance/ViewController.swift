5299adbf1e71b72f9fcb651e4e16f1ac2d0f1498
Max Okuneev <okuneev@gmail.com>
2019-12-15 12:21:26 +0700

    Initial Commit

---
+++ My Finance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Finance
+//
+//  Created by Максим Окунеев on 12/15/19.
+//  Copyright © 2019 Максим Окунеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
